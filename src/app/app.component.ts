d4b68e1b0bf239ea9e236cd582b9ecfc55822e73
Gisela Lanciani <giselalanciani@gmail.com>
2023-04-05 11:50:28 -0300

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gise-library';
+
+  @ViewChild('drawer')
+  drawer: any;
+
+  toggleSidenav () {
+    console.log('si apretó');
+    this.drawer.toggle()
+    console.log('funciono con un viewchild');
+  }
 }
