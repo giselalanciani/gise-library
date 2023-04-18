9f77e179901d8ad6d0f524a7446f8bdad35519fb
Gisela Lanciani <giselalanciani@gmail.com>
2023-04-18 17:41:15 -0300

    src/app/books/books.module.ts

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { BooksRoutingModule } from './books-routing.module';
-import { BooksComponent } from './books.component';
+import { BooksRoutingModule } from './books-routing.module'
+import { BooksComponent } from './books/books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    BooksComponent
+    BooksComponent,
+    BookDetailsComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     BooksRoutingModule
   ]
 })
