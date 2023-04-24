c53cbe48d2180f7632eb4b15670b3bb5a08b43c4
Gisela Lanciani <giselalanciani@gmail.com>
2023-04-24 17:38:55 -0300

    src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,29 +7,26 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import { CategoriesComponent } from './categories2/categories.component'
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { CategoriesComponent } from './categories2/categories.component';
 import { CategoryDetailComponent } from './category-detail/category-detail.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, CategoriesComponent, CategoryDetailComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    CategoriesComponent,
+    CategoryDetailComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports:[]
+  exports: [],
 })
 export class AppModule {}
