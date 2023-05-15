24aff7b97d7cc563eebb7d6ac1ae02b99ec498ed
Gisela Lanciani <giselalanciani@gmail.com>
2023-05-15 10:27:26 -0300

    add country lists

--- src/app/countries/countries/countries.component.ts
+++ src/app/countries/countries/countries.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ICountry } from '../models';
+import { CountriesService } from '../services/countries.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit {
+  countryList$: Observable<ICountry[]>;
+  countryList: ICountry[] = [];
+  countryListSubscription!: Subscription;
 
+  columnsToDisplay = ['name'];
+
+  constructor(public countryServices: CountriesService) {
+    this.countryList$ = this.countryServices.getCountries();
+  }
+
+  ngOnInit(): void {
+    this.countryListSubscription = this.countryList$.subscribe((countries) => {
+      this.countryList = countries;
+    });
+  }
 }
