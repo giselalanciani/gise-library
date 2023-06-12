bf0d4a7afe0ac55b6f7c63203f6d43febfce9944
Gisela Lanciani <giselalanciani@gmail.com>
2023-06-12 14:56:30 -0300

    add filtered countries and sort table

--- src/app/countries/countries-v3/countries-v3.component.ts
+++ src/app/countries/countries-v3/countries-v3.component.ts
@@ -14,9 +14,15 @@ import { CountriesService } from '../services/countries.service';
 export class CountriesV3Component implements OnInit, OnDestroy {
   countryList$: Observable<ICountry[]>;
   countryList: ICountry[] = [];
+  filteredCountryList: ICountry[] = [];
   countryListSubscription!: Subscription;
   removeCountrySubscription!: Subscription;
   dialogDeleteSubscription!: Subscription;
+
+  sortState: { column: string; order: 'asc' | 'desc' } = {
+    column: '',
+    order: 'desc',
+  };
 
   constructor(
     public countryServices: CountriesService,
@@ -30,7 +36,39 @@ export class CountriesV3Component implements OnInit, OnDestroy {
     this.countryListSubscription = this.activatedRouteService.data.subscribe(
       (data) => {
         this.countryList = data['countriesList'];
+        this.filteredCountryList = data['countriesList'];
       }
+    );
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+
+    this.filteredCountryList = this.countryList.filter((country) =>
+      country.name.toLowerCase().includes(filterValue)
+    );
+  }
+  sortTable(event: Event) {
+    const element = event.target as HTMLTableCellElement;
+    const columnName = element.getAttribute('name');
+
+    switch (columnName) {
+      case 'name':
+        this.sortState = {
+          column: 'name',
+          order: this.sortState.order === 'desc' ? 'asc' : 'desc',
+        };
+        this.sortByPropertyName(this.sortState.order);
+        break;
+      default:
+        break;
+    }
+  }
+
+  sortByPropertyName(order: 'asc' | 'desc') {
+    this.filteredCountryList.sort((a, b) =>
+      order === 'asc'
+        ? a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+        : b.name.toLowerCase().localeCompare(a.name.toLowerCase())
     );
   }
 
@@ -57,6 +95,7 @@ export class CountriesV3Component implements OnInit, OnDestroy {
                 this.countryListSubscription = this.countryList$.subscribe(
                   (countries) => {
                     this.countryList = countries;
+                    this.filteredCountryList = countries;
                   }
                 );
               });
